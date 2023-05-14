1ca3dbbda3a4ef3b15ac5c2e0f06571cc59d4e96
devaguava <117663776+devaguava@users.noreply.github.com>
2023-05-14 23:52:05 +0700

    menambahkan show banner menggunakan teknik table-driven construction dan runtime configuration

---
+++ WebApplication1/Banner.cs
@@ -0,0 +1,36 @@
+﻿namespace DBServerAPI
+{
+    public class Banner
+    {
+        // Table-Driven method
+        public enum banner
+        {
+            PenggunaanWebsite,
+            PerbaikanKTM,
+            PenggantianKTM
+        }
+
+        public void showBanner(banner x)
+        {
+            //i.s. Terdapat parameter x sebagai indeks untuk enum banner
+            //f.s. Menampilkan imageBanner indeks yang sesuai dengan parameter
+            string[] imageBanner = { "imagePenggunaanWebsite.jpg", "imagePerbaikanKTM.jpg", "imagePenggantianKTM.jpg" };
+            Console.WriteLine(imageBanner[(int)x]);
+        }
+
+        // Runtime Configuration
+        // class untuk menampung configurasi
+        public string PenggunaanWebsite { get; set; }
+        public string PerbaikanKTM { get; set; }
+        public string PenggantianKTM { get; set; }
+
+        public Banner() { }
+
+        public Banner(string penggunaanWebsite, string perbaikanKTM, string penggantianKTM)
+        {
+            PenggunaanWebsite = penggunaanWebsite;
+            PerbaikanKTM = perbaikanKTM;
+            PenggantianKTM = penggantianKTM;
+        }
+    }
+}
