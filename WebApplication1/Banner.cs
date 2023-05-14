fc1a2ab6dceac625dbc8340702ca89b716c753c6
Regyyyy <regyrahman@gmail.com>
2023-05-14 15:53:59 +0700

    Menambahkan Class Banner
    Class banner serta method showBanner yang akan menampilkan image banner sesuai parameternya.

---
+++ WebApplication1/Banner.cs
@@ -0,0 +1,18 @@
+﻿namespace DBServerAPI
+{
+    public class Banner
+    {
+        public enum banner
+        {
+            PenggunaanWebsite,
+            PerbaikanKTM,
+            PenggantianKTM
+        } 
+
+        public void showBanner(banner x)
+        {
+            String[] imageBanner = { "image1.jpg", "image2.jpg", "image3.jpg" };
+            Console.WriteLine(imageBanner[(int)x]);
+        }
+    }
+}
