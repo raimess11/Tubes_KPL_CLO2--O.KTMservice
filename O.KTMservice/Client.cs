5607ead5e55f488e8457250e69d8648a6a2f8591
raimess11 <rahma.sakti24@gmail.com>
2023-06-16 23:03:47 +0700

    Create Client.cs

---
+++ O.KTMservice/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBServerAPI;
+
+namespace O.KTMservice
+{
+    internal class Client
+    {
+        public static Mahasiswa mahasiswa;
+
+        public static void run()
+        {
+            // panggil GUI pertama yang akan ditampilkan
+        }
+    }
+}
