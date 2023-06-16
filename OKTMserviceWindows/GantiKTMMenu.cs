3b658f05afa8b6df7e5d5058aaf6fbc5b6801941
raimess11 <rahma.sakti24@gmail.com>
2023-06-16 22:54:35 +0700

    merapikan code

--- OKTMserviceWindows/GantiKTMMenu.cs
+++ OKTMserviceWindows/GantiKTMMenu.cs
@@ -17,29 +17,10 @@ namespace OKTMserviceWindows
             InitializeComponent();
         }
 
-        private void _labelColon1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _labelTahunAjar_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _labelProgramStudi_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-
+            // membuat request
+            // mengupload ke server menggunakan API
         }
     }
 }
