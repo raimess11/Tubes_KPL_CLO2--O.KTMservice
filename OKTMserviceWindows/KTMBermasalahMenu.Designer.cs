107812bf2808965b38218026a229debb543d2673
raimess11 <rahma.sakti24@gmail.com>
2023-06-16 22:07:54 +0700

    KTM bermasalah view

---
+++ OKTMserviceWindows/KTMBermasalahMenu.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace OKTMserviceWindows
+{
+    partial class KTMBermasalahMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            _labelNamaMahasiswa = new Label();
+            _ValueNameMahasiswa = new Label();
+            _labelNIM = new Label();
+            _NIMValue = new Label();
+            _labelTahunAjar = new Label();
+            _labelProgramStudi = new Label();
+            _labelUploadKSM = new Label();
+            _labelUploadKTM = new Label();
+            _labelColon1 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // _labelNamaMahasiswa
+            // 
+            _labelNamaMahasiswa.AutoSize = true;
+            _labelNamaMahasiswa.Location = new Point(38, 38);
+            _labelNamaMahasiswa.Name = "_labelNamaMahasiswa";
+            _labelNamaMahasiswa.Size = new Size(100, 15);
+            _labelNamaMahasiswa.TabIndex = 0;
+            _labelNamaMahasiswa.Text = "Nama Mahasiswa";
+            _labelNamaMahasiswa.UseWaitCursor = true;
+            // 
+            // _ValueNameMahasiswa
+            // 
+            _ValueNameMahasiswa.AutoSize = true;
+            _ValueNameMahasiswa.Location = new Point(183, 38);
+            _ValueNameMahasiswa.Name = "_ValueNameMahasiswa";
+            _ValueNameMahasiswa.Size = new Size(67, 15);
+            _ValueNameMahasiswa.TabIndex = 1;
+            _ValueNameMahasiswa.Text = "NameValue";
+            // 
+            // _labelNIM
+            // 
+            _labelNIM.AutoSize = true;
+            _labelNIM.Location = new Point(454, 38);
+            _labelNIM.Name = "_labelNIM";
+            _labelNIM.Size = new Size(30, 15);
+            _labelNIM.TabIndex = 0;
+            _labelNIM.Text = "NIM";
+            _labelNIM.UseWaitCursor = true;
+            // 
+            // _NIMValue
+            // 
+            _NIMValue.AutoSize = true;
+            _NIMValue.Location = new Point(522, 38);
+            _NIMValue.Name = "_NIMValue";
+            _NIMValue.Size = new Size(58, 15);
+            _NIMValue.TabIndex = 1;
+            _NIMValue.Text = "NIMValue";
+            // 
+            // _labelTahunAjar
+            // 
+            _labelTahunAjar.AutoEllipsis = true;
+            _labelTahunAjar.AutoSize = true;
+            _labelTahunAjar.Location = new Point(38, 74);
+            _labelTahunAjar.Name = "_labelTahunAjar";
+            _labelTahunAjar.Size = new Size(63, 15);
+            _labelTahunAjar.TabIndex = 0;
+            _labelTahunAjar.Text = "Tahun Ajar";
+            _labelTahunAjar.UseWaitCursor = true;
+            _labelTahunAjar.Click += _labelTahunAjar_Click;
+            // 
+            // _labelProgramStudi
+            // 
+            _labelProgramStudi.AutoEllipsis = true;
+            _labelProgramStudi.AutoSize = true;
+            _labelProgramStudi.Location = new Point(38, 107);
+            _labelProgramStudi.Name = "_labelProgramStudi";
+            _labelProgramStudi.Size = new Size(83, 15);
+            _labelProgramStudi.TabIndex = 0;
+            _labelProgramStudi.Text = "Program Studi";
+            _labelProgramStudi.UseWaitCursor = true;
+            _labelProgramStudi.Click += _labelProgramStudi_Click;
+            // 
+            // _labelUploadKSM
+            // 
+            _labelUploadKSM.AutoEllipsis = true;
+            _labelUploadKSM.AutoSize = true;
+            _labelUploadKSM.Location = new Point(38, 144);
+            _labelUploadKSM.Name = "_labelUploadKSM";
+            _labelUploadKSM.Size = new Size(72, 15);
+            _labelUploadKSM.TabIndex = 0;
+            _labelUploadKSM.Text = "Upload KSM";
+            _labelUploadKSM.UseWaitCursor = true;
+            // 
+            // _labelUploadKTM
+            // 
+            _labelUploadKTM.AutoEllipsis = true;
+            _labelUploadKTM.AutoSize = true;
+            _labelUploadKTM.Location = new Point(38, 183);
+            _labelUploadKTM.Name = "_labelUploadKTM";
+            _labelUploadKTM.Size = new Size(72, 15);
+            _labelUploadKTM.TabIndex = 0;
+            _labelUploadKTM.Text = "Upload KTM";
+            _labelUploadKTM.UseWaitCursor = true;
+            // 
+            // _labelColon1
+            // 
+            _labelColon1.AutoEllipsis = true;
+            _labelColon1.AutoSize = true;
+            _labelColon1.Location = new Point(155, 74);
+            _labelColon1.Name = "_labelColon1";
+            _labelColon1.Size = new Size(10, 15);
+            _labelColon1.TabIndex = 0;
+            _labelColon1.Text = ":";
+            _labelColon1.UseWaitCursor = true;
+            _labelColon1.Click += _labelColon1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoEllipsis = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(155, 107);
+            label1.Name = "label1";
+            label1.Size = new Size(10, 15);
+            label1.TabIndex = 0;
+            label1.Text = ":";
+            label1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            label2.AutoEllipsis = true;
+            label2.AutoSize = true;
+            label2.Location = new Point(155, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(10, 15);
+            label2.TabIndex = 0;
+            label2.Text = ":";
+            label2.UseWaitCursor = true;
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoEllipsis = true;
+            label3.AutoSize = true;
+            label3.Location = new Point(155, 183);
+            label3.Name = "label3";
+            label3.Size = new Size(10, 15);
+            label3.TabIndex = 0;
+            label3.Text = ":";
+            label3.UseWaitCursor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(171, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(171, 104);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(467, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(171, 141);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(164, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(171, 180);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(164, 23);
+            textBox4.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(299, 234);
+            button1.Name = "button1";
+            button1.Size = new Size(139, 23);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // KTMBermasalahMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(706, 406);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(_NIMValue);
+            Controls.Add(_labelNIM);
+            Controls.Add(_ValueNameMahasiswa);
+            Controls.Add(_labelUploadKTM);
+            Controls.Add(_labelProgramStudi);
+            Controls.Add(_labelUploadKSM);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(_labelColon1);
+            Controls.Add(_labelTahunAjar);
+            Controls.Add(_labelNamaMahasiswa);
+            Name = "KTMBermasalahMenu";
+            Text = "KTMBermasalahMenu";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label _labelNamaMahasiswa;
+        private Label _ValueNameMahasiswa;
+        private Label _labelNIM;
+        private Label _NIMValue;
+        private Label _labelTahunAjar;
+        private Label _labelProgramStudi;
+        private Label _labelUploadKSM;
+        private Label _labelUploadKTM;
+        private Label _labelColon1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+    }
+}
---
+++ OKTMserviceWindows/KTMBermasalahMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OKTMserviceWindows
+{
+    public partial class KTMBermasalahMenu : Form
+    {
+        public KTMBermasalahMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void _labelColon1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _labelTahunAjar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _labelProgramStudi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
